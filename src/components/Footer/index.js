4850c6e385bd8482704babb31193c53838473a94
Janneth Verduzco <janneth_verduzco@live.com>
2021-03-21 00:22:03 -0500

    added footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <span>JAV © Copyright</span>
+    </footer>
+  );
+}
+
+export default Footer;
